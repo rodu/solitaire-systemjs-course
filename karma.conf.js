2973fce296ffe7ee73d416015cafd801f5fa13b9
Roberto Schioppa <roberto.schioppa@propylon.com>
2016-08-16 12:08:41 +0100

    Adds configuration for karma via jspm

--- karma.conf.js
+++ karma.conf.js
@@ -12,26 +12,50 @@ module.exports = function (config) {
       "app/bower_components/angular-route/angular-route.js",
       "app/bower_components/angular-mocks/angular-mocks.js",
       "app/bower_components/underscore/underscore.js",
-      "app/cards/**/*.js",
       "app/klondike/piles/pile.js",
       "app/klondike/piles/foundationPile.js",
       "app/klondike/piles/remainderPile.js",
       "app/klondike/piles/tableauPile.js",
-      "app/klondike/*.js",
-      "tests/**/*.js"
     ],
+
+    proxies: {
+      // Whenever during tests execution Karma looks for a node_modules, this
+      // will be resolved though base/ which is where Karma is serving files
+      // from
+      "/node_modules": "/base/node_modules"
+    },
+
+    jspm: {
+      // Tells where the config file for SystemJS is
+      config: "app/config.js",
+      // Points to a folder that contains a copy of system.js to use that for
+      // testing
+      packages: "app/bower_components/system.js/dist",
+      // A set of files Karma will serve but will not included in the test page,
+      // not loaded through SystemJS either.
+      serveFiles: [
+        "app/**/*.js",
+        "node_modules/babel-core/lib/api/browser.js"
+      ],
+      // Treats the files here as modules to be imported through the
+      // System.import instead of being just scripts like the files above
+      loadFiles: [
+        "tests/cards/card_test.js"
+      ]
+    },
 
     autoWatch: true,
 
-    frameworks: ["jasmine"],
+    frameworks: ["jspm", "jasmine"],
 
-    browsers: ["Chrome", "Firefox"],
+    browsers: ["Chrome"],
 
     plugins: [
       "karma-chrome-launcher",
       "karma-firefox-launcher",
       "karma-jasmine",
-      "karma-junit-reporter"
+      "karma-junit-reporter",
+      "karma-jspm"
     ],
 
     junitReporter: {
